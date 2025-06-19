391640e296eb7f3621863d03501809d7cec7b148
Abdulrahman mohammed <abdo.lazorde@gmail.com>
2025-06-19 08:41:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -145,7 +145,7 @@ export default function Header() {
   const showDashboardBarcodeScanner = pathname === '/dashboard' && hasRole(['admin', 'employee', 'employee_return']);
   const showHeaderAddProduct = pathname === '/dashboard' && hasRole(['admin']);
   
-  const userAvatarPlaceholder = `https://placehold.co/40x40/A4C3F5/FFFFFF?text=${getInitials(currentUser?.username || 'N A')}&font=sans-serif`;
+  const userAvatarSrc = "https://images.unsplash.com/photo-1633409361618-c73427e4e206?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxpY29ufGVufDB8fHx8MTc1MDMyMjQ3M3ww&ixlib=rb-4.1.0&q=80&w=1080";
 
 
   return (
@@ -242,7 +242,7 @@ export default function Header() {
               <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-9 w-9 rounded-full">
                   <Avatar className="h-9 w-9">
-                    <AvatarImage src={userAvatarPlaceholder} alt={currentUser.username} data-ai-hint="user avatar placeholder"/>
+                    <AvatarImage src={userAvatarSrc} alt={currentUser.username} data-ai-hint="user avatar placeholder"/>
                     <AvatarFallback>{getInitials(currentUser.username)}</AvatarFallback>
                   </Avatar>
                 </Button>
@@ -268,3 +268,4 @@ export default function Header() {
     </TooltipProvider>
   );
 }
+
