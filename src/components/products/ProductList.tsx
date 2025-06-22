73382529f5ea6cb9adc11a1c994eff21654f93f8
Abdulrahman mohammed <abdo.lazorde@gmail.com>
2025-06-22 10:39:32 +0000

    كلمة  50 LYD صغرها قليلا في صفحة المنتج الصور

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -111,7 +111,7 @@ export default function ProductList({ searchTerm }: ProductListProps) {
               <CardContent className="p-3 flex-grow flex flex-col justify-between">
                 <div>
                   <h3 className="font-semibold text-xs truncate" title={product.name}>{product.name}</h3>
-                  <p className="font-semibold text-sm text-primary mt-1.5"><span className="text-muted-foreground font-normal text-xs">السعر: </span>{product.price} LYD</p>
+                  <p className="font-semibold text-xs text-primary mt-1.5"><span className="text-muted-foreground font-normal text-xs">السعر: </span>{product.price} LYD</p>
                 </div>
                 <div className="flex justify-between items-center mt-2.5">
                   <Badge 
